a211a20e8a7b2e1a51a9f738655492cf4f1bd629
samirdev3 <72462244+samirdev3@users.noreply.github.com>
2021-04-13 17:29:45 +0530

    Add files via upload

---
+++ README.md
@@ -0,0 +1,9 @@
+## Side Sticky ADs for WordPress
+With the help of this plugin you can place sticky Ads on both side of the website (box layout).
+Note: Works only for the desktop.
+
+
+== Changelog ==
+
+= 1.0.1 (12th April 2021) =
+* plugin build
---
+++ options.php
@@ -0,0 +1,64 @@
+<?php
+/*
+   Plugin Name: Side Sticky ADs for WordPress
+   Plugin URI: -
+   description: With the help of this plugin you can place sticky Ads on both side of the website (box layout).
+   Version: 1.0.1
+   Author: Samir
+   Author URI: https://github.com/samirdev3
+   License: GPL2+
+*/
+
+// Exit if accessed directly.
+if ( ! defined( 'ABSPATH' ) ) exit;
+
+if(!function_exists('awt_sticky_ad_code')){
+    add_action('wp_footer', 'awt_sticky_ad_code', 1000);
+    function awt_sticky_ad_code(){
+        if(!wp_is_mobile()){
+        ?>
+            <style>.awt_side_sticky{position:fixed;top:5px;width:160px;height:600px;opacity:0;visibility:hidden;z-index:99998}.awt_side_sticky .revenue{display:block;width:160px;height:600px;background:#eee}</style>
+            <div id="awt-left" class="awt_side_sticky">
+                <div class="revenue">
+                    <!-- LHS ad code here -->
+                </div>
+            </div>
+            <div id="awt-right" class="awt_side_sticky">
+                <div class="revenue">
+                    <!-- RHS ad code here -->
+                </div>
+            </div>
+            <script>
+                const containerClass = '.tdc-content-wrap'; //Newspaper
+                window.onresize = awtAdCodeFunction;
+                window.onload = awtAdCodeFunction;
+                function awtAdCodeFunction(){
+                    const   containerW = document.querySelector(containerClass).clientWidth;
+                    let     offSet = (document.body.offsetWidth/2) - ((containerW/2) + 165),
+                            mSign = Math.sign(offSet);
+                    document.getElementById('awt-left').style.left = `${offSet}px`;
+                    document.getElementById('awt-right').style.right = `${offSet}px`;
+                    awtDisplayAd(mSign);
+                }
+
+                function awtDisplayAd(mSign){
+                    const adClass = document.querySelectorAll('.awt_side_sticky');
+                    if(mSign === 1 && (window.innerHeight) >= 610){
+                        for(let $i=0;$i<adClass.length;$i++){
+                            adClass[$i].style.opacity = 1;
+                            adClass[$i].style.visibility = 'visible';
+                        }
+                    }else{
+                        for(let $i=0;$i<adClass.length;$i++){
+                            adClass[$i].style.opacity = 0;
+                            adClass[$i].style.visibility = 'hidden';
+                        }
+                    }
+                }
+            </script>
+        <?php
+        }
+    }
+}
+
+?>
